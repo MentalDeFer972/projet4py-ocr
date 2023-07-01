73115571f2c1a1e861c7c0ba5bacf5010745dbae
joffr <joffreyterrine@protonmail.ocm>
2023-07-01 12:07:37 +0200

    commit10

--- vues/menu.py
+++ vues/menu.py
@@ -1,37 +1,55 @@
 from controleurs.application import *
 
+
 class State:
     def __init__(self):
         pass
 
-    def display_menu(self,nombre):
+    def display_menu(self, nombre):
         if nombre == 1:
-            print("MENU PRINCIPAL\n 1-Gérer les Joueurs \n 2-Gérer les tournois \n 3-Gérer Tours et Matchs \n 4-Faire des Rapports")
+            print(
+                "MENU PRINCIPAL\n "
+                "1-Gérer les Joueurs \n "
+                "2-Gérer les tournois \n "
+                "3-Gérer Tours et Matchs \n "
+                "4-Faire des Rapports")
             saisie = int(input())
             return saisie
         if nombre == 2:
-            print("GERER UN JOUEUR \n 1-Ajouter un joueur \n 2-Supprimer un joueur \n 3-Modifier un joueur \n 4-Retour")
+            print("GERER UN JOUEUR \n "
+                  "1-Ajouter un joueur \n "
+                  "2-Supprimer un joueur \n "
+                  "3-Modifier un joueur \n "
+                  "4-Retour")
             saisie = int(input())
             return saisie
         if nombre == 3:
-            print("GERER UN TOURNOI \n 1-Ajouter un tournoi \n 2-Supprimer un tournoi \n 3-Modifier un tournoi \n 4-Retour")
+            print(
+                "GERER UN TOURNOI \n "
+                "1-Ajouter un tournoi \n "
+                "2-Supprimer un tournoi \n "
+                "3-Modifier un tournoi \n "
+                "4-Retour")
             saisie = int(input())
             return saisie
         if nombre == 4:
-            print("GERER UN TOUR/MATCH \n 1-Ajouter un tour \n 2-Supprimer un tour \n 3-Modifier un tour \n 4-Retour")
+            print("GERER UN TOUR/MATCH \n "
+                  "1-Ajouter un tour \n "
+                  "2-Supprimer un tour \n "
+                  "3-Modifier un tour \n "
+                  "4-Retour")
             saisie = int(input())
             return saisie
         if nombre == 5:
             print("FAIRE DES RAPPORTS\n"
-                    "1-liste de tous les joueurs par ordre alphabétique\n"
-                    "2-liste de tous les tournois\n"
-                    "3-nom et dates d’un tournoi donné\n"
-                    "4-liste des joueurs du tournoi par ordre alphabétique\n"
-                    "5-liste de tous les tours du tournoi et de tous les matchs du tour\n"
-                    "6-Retour")
+                  "1-liste de tous les joueurs par ordre alphabétique \n"
+                  "2-liste de tous les tournois \n"
+                  "3-nom et dates d’un tournoi donné \n"
+                  "4-liste des joueurs du tournoi par ordre alphabétique \n"
+                  "5-liste de tous les tours du tournoi et de tous les matchs du tour \n"
+                  "6-Retour")
             saisie = int(input())
             return saisie
-
 
     def gestion_menu(self):
         saisie = self.display_menu(1)
@@ -54,10 +72,11 @@ class State:
             UserManager.supprimer_joueur()
         elif saisie == 3:
             pass
-        elif saisie == 4 :
+        elif saisie == 4:
             self.gestion_menu()
         else:
             self.gerer_joueur()
+
     def gerer_tournoi(self):
         saisie = self.display_menu(3)
         if saisie == 1:
@@ -70,6 +89,7 @@ class State:
             self.gestion_menu()
         else:
             self.gerer_tournoi()
+
     def gerer_tour_match(self):
         saisie = self.display_menu(4)
         if saisie == 1:
@@ -99,15 +119,3 @@ class State:
             self.gestion_menu()
         else:
             self.gerer_rapports()
-
-
-
-
-
-
-
-
-
-
-
-

1ef03f1b001423ecf737059d5b34bcec8394db39
joffr <joffreyterrine@protonmail.ocm>
2023-07-06 22:30:28 +0200

    commit12

--- controleurs/application.py
+++ controleurs/application.py
@@ -12,12 +12,10 @@ class UserManager:
     def extraire_informations_joueur(self):
         with open('joueur.json','r') as file:
             data = json.load(file)
-            num_joueur = data['num_joueur']
-            num_club = data['num_club']
             nom_famille = data['nom_famille']
             prenom = data['prenom']
             date_naissance = data['date_naissance']
-            print(f" Num Joueur: {num_joueur} , Num Club: {num_club} , Nom Famille: {nom_famille} , Prenom: {prenom}")
+            print(f"Nom Famille: {nom_famille} , Prenom: {prenom} , Date de naissance: {date_naissance}")
 
 class TournamentManager:
     def supprimer_tournoi(self):
